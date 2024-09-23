ec1743a7f4d8a013d2212fa7fef69783b8375ca3
Berkay Yelken <berkayelken@gmail.com>
2024-09-23 14:55:43 +0300

    art-gallery component was created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { ArtGalleryComponent } from './art-gallery/art-gallery.component';
 
 export const routes: Routes = [{ path: 'home', component: HomeComponent },
     { path: 'sign-in', component: SignInComponent },
     { path: 'sign-up', component: SignUpComponent },
+    { path: 'art-gallery', component: ArtGalleryComponent },
     { path: '**', component: HomeComponent }
 ];
