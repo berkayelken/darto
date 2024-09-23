2abf6b7ea77f567b9c0244775ecf03859468ed18
Berkay Yelken <berkayelken@gmail.com>
2024-09-23 12:03:57 +0300

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: 'home', component: HomeComponent },
+    { path: 'sign-in', component: SignInComponent },
+    { path: '**', component: HomeComponent }
+];
