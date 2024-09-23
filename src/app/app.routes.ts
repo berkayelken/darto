3ea2a03889dc8e5a28878dbb4e94e639e769dddd
Berkay Yelken <berkayelken@gmail.com>
2024-09-23 14:25:17 +0300

    sign up component generation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 export const routes: Routes = [{ path: 'home', component: HomeComponent },
     { path: 'sign-in', component: SignInComponent },
+    { path: 'sign-up', component: SignUpComponent },
     { path: '**', component: HomeComponent }
 ];
